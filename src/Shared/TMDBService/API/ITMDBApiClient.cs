71726e70fe6e7573964b5e2590fea4d3e29b824d
Henrik Fröhling <henrik.froehling@gmail.com>
2021-03-21 00:02:26 +0100

    Remove default values for tmdb api parameters

--- src/Shared/TMDBService/API/ITMDBApiClient.cs
+++ src/Shared/TMDBService/API/ITMDBApiClient.cs
@@ -1,4 +1,4 @@
-using Models.TMDBApi;
+﻿using Models.TMDBApi;
 using Refit;
 using System.Threading.Tasks;
 
@@ -12,35 +12,35 @@ namespace TMDBService.API
         // genres
 
         [Get("/genre/tv/list?api_key={apiKey}&language={languageCode}")]
-        Task<TMDBGenresResponse> GetShowGenresAsync(string apiKey, string languageCode = "de-DE");
+        Task<TMDBGenresResponse> GetShowGenresAsync(string apiKey, string languageCode);
 
         [Get("/genre/movie/list?api_key={apiKey}&language={languageCode}")]
-        Task<TMDBGenresResponse> GetMovieGenresAsync(string apiKey, string languageCode = "de-DE");
+        Task<TMDBGenresResponse> GetMovieGenresAsync(string apiKey, string languageCode);
 
         // trending
 
         [Get("/trending/tv/day?api_key={apiKey}&language={languageCode}&page={page}")]
-        Task<TMDBListResponse<TMDBTrendingShowResponse>> GetTrendingShowsAsync(string apiKey, string languageCode = "de-DE", int page = 1);
+        Task<TMDBListResponse<TMDBTrendingShowResponse>> GetTrendingShowsAsync(string apiKey, string languageCode, int page);
 
         [Get("/trending/movie/day?api_key={apiKey}&language={languageCode}&page={page}")]
-        Task<TMDBListResponse<TMDBTrendingMovieResponse>> GetTrendingMoviesAsync(string apiKey, string languageCode = "de-DE", int page = 1);
+        Task<TMDBListResponse<TMDBTrendingMovieResponse>> GetTrendingMoviesAsync(string apiKey, string languageCode, int page);
 
         // discoverable
 
         [Get("/discover/tv?api_key={apiKey}&language={languageCode}&watch_region={watchRegionCode}&with_genres={withGenres}&page={page}")]
-        Task<TMDBListResponse<TMDBDiscoverableShowResponse>> GetDiscoverableShowsByGenresAsync(string apiKey, string withGenres, int page = 1,
-                                                                                               string languageCode = "de-DE", string watchRegionCode = "DE");
+        Task<TMDBListResponse<TMDBDiscoverableShowResponse>> GetDiscoverableShowsByGenresAsync(string apiKey, string withGenres, int page,
+                                                                                               string languageCode, string watchRegionCode);
 
         [Get("/discover/movie?api_key={apiKey}&language={languageCode}&watch_region={watchRegionCode}&with_genres={withGenres}&page={page}")]
-        Task<TMDBListResponse<TMDBDiscoverableMovieResponse>> GetDiscoverableMoviesByGenresAsync(string apiKey, string withGenres, int page = 1,
-                                                                                                 string languageCode = "de-DE", string watchRegionCode = "DE");
+        Task<TMDBListResponse<TMDBDiscoverableMovieResponse>> GetDiscoverableMoviesByGenresAsync(string apiKey, string withGenres, int page,
+                                                                                                 string languageCode, string watchRegionCode);
 
         [Get("/discover/tv?api_key={apiKey}&language={languageCode}&watch_region={watchRegionCode}&with_keywords={withKeywords}&page={page}")]
-        Task<TMDBListResponse<TMDBDiscoverableShowResponse>> GetDiscoverableShowsByKeywordsAsync(string apiKey, string withKeywords, int page = 1,
-                                                                                                 string languageCode = "de-DE", string watchRegionCode = "DE");
+        Task<TMDBListResponse<TMDBDiscoverableShowResponse>> GetDiscoverableShowsByKeywordsAsync(string apiKey, string withKeywords, int page,
+                                                                                                 string languageCode, string watchRegionCode);
 
         [Get("/discover/movie?api_key={apiKey}&language={languageCode}&watch_region={watchRegionCode}&with_keywords={withKeywords}&page={page}")]
-        Task<TMDBListResponse<TMDBDiscoverableMovieResponse>> GetDiscoverableMoviesByKeywordsAsync(string apiKey, string withKeywords, int page = 1,
-                                                                                                   string languageCode = "de-DE", string watchRegionCode = "DE");
+        Task<TMDBListResponse<TMDBDiscoverableMovieResponse>> GetDiscoverableMoviesByKeywordsAsync(string apiKey, string withKeywords, int page,
+                                                                                                   string languageCode, string watchRegionCode);
     }
 }
