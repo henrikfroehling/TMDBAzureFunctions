a2966c91ed2a55440bfc934f29b8845bf44cd8f1
Henrik Fröhling <henrik.froehling@gmail.com>
2021-03-23 01:21:57 +0100

    Fix compression and decompression

--- src/Shared/CompressionService/CompressionServiceImpl.cs
+++ src/Shared/CompressionService/CompressionServiceImpl.cs
@@ -10,11 +10,12 @@ namespace CompressionService
         public string CompressJSONAsBase64(string json)
         {
             byte[] jsonData = Encoding.UTF8.GetBytes(json);
-            using var memory = new MemoryStream(jsonData);
-            using var compressedMemory = new MemoryStream();
-            using var gzip = new GZipStream(compressedMemory, CompressionMode.Compress);
-            memory.CopyTo(gzip);
-            byte[] compressedData = compressedMemory.ToArray();
+            using var input = new MemoryStream(jsonData);
+            using var output = new MemoryStream();
+            using var gzip = new GZipStream(output, CompressionMode.Compress);
+            input.CopyTo(gzip);
+            gzip.Close();
+            byte[] compressedData = output.ToArray();
             return Convert.ToBase64String(compressedData);
         }
 
@@ -26,11 +27,12 @@ namespace CompressionService
 
         public string DecompressToJson(byte[] compressedData)
         {
-            using var memory = new MemoryStream(compressedData);
-            using var gzip = new GZipStream(memory, CompressionMode.Decompress);
-            using var memoryBuffer = new MemoryStream();
-            gzip.CopyTo(memoryBuffer);
-            byte[] jsonData = memoryBuffer.ToArray();
+            using var input = new MemoryStream(compressedData);
+            using var gzip = new GZipStream(input, CompressionMode.Decompress);
+            using var output = new MemoryStream();
+            gzip.CopyTo(output);
+            gzip.Close();
+            byte[] jsonData = output.ToArray();
             return Encoding.UTF8.GetString(jsonData);
         }
     }
